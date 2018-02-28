4e27b36b5b8562e7c0df8c18b77ab63b33a31d25
srajaram93 <srajaram93@gmail.com>
2018-02-28 14:29:40 -0500

    Added error logging and  initializing session

--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
-//error_reporting(-1);
-//ini_set('display_errors', true);
+session_start();
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+error_log("Hello, errors!");
+
 include ('client.php');
 $user = $_POST['user'];
 $pass = $_POST['pass'];
@@ -13,5 +18,7 @@ if($response == false)
   else
   {
   echo "Login Successful!";
+  $_SESSION['user'] = $user;
+  echo $_SESSION['user'];
   }
 ?>
