5fa0c6c49f8fb9868c3c00b0d45b889fc22861eb
srajaram93 <srajaram93@gmail.com>
2018-02-28 14:26:28 -0500

    Added error logging

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 #!/usr/bin/php
 <?php
+
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+error_log("Hello, errors!");
+
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
