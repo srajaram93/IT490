09ac8183f02430d5303e9e533164cf2b192badf3
srajaram93 <srajaram93@gmail.com>
2018-03-01 18:29:25 -0500

    added minor changes to appearance

--- registration.php
+++ registration.php
@@ -1,6 +1,9 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', false);
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+error_log("Hello, errors!");
 
 include ('client.php');
 $email = $_POST['email'];
@@ -11,9 +14,11 @@ $response = registration($email,$firstname,$lastname,$password);
 if($response != true)
   {
     echo "Registration Failed. Please try a different email";
+    header( "Refresh:2; url=registration.html", true, 303);
   }
   else
   {
     echo "Thank you for Registering!";
+    header( "Refresh:2; url=login.html", true, 303);
   }
 ?>
