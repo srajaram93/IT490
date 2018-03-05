fed67905b156aff291347e114b6c43366f87af12
srajaram93 <srajaram93@gmail.com>
2018-03-05 12:57:32 -0500

    added error logging

--- location.php
+++ location.php
@@ -1,6 +1,9 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', true);
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
 include ('client.php');
 
 $zipcode = $_GET['u'];
